b66463f2bcf8334dfe4ef771e6d1631a34c61998
Shubham Parashar <shubhamparashar210@gmail.com>
2021-10-04 11:13:34 +0530

    Heroku api is added to code

---
+++ client/build/precache-manifest.4ae02d11027defe93f7f30ed29699876.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4378cdd82eecd7c889c03366fb3b790",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5e1d402b9e04bbfc41c",
+    "url": "/static/css/main.154db6b3.chunk.css"
+  },
+  {
+    "revision": "a5c2035dd483bb7f3572",
+    "url": "/static/js/2.3a9538ab.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.3a9538ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5e1d402b9e04bbfc41c",
+    "url": "/static/js/main.6b53dff1.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
